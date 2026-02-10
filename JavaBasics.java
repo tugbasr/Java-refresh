bb6355126aa17999f65036dd12a77bb2e949ac38
tugbasr <112336692+tugbasr@users.noreply.github.com>
2026-02-10 13:25:16 -0500

    Initial JavaBasics class commit

---
+++ JavaBasics.java
@@ -0,0 +1,22 @@
+public class JavaBasics {
+  public static void main(String[] args) {
+    int a = 5;
+    int b = 3;
+    int sum = add(a,b);
+    Sytem.out.println("Sum: " +sum);
+
+    if(sum > 5) {
+      Sytem.out.println("Sum is greater than 5");
+    }else {
+      Sytem.out.println("Sum is 5 or less");
+    }
+
+    for(int i = 0 ; i < 3; i++){
+      Sytem.out.println("Iteration: " + i);
+    }
+  }
+
+  public static int add(int x, int y) {
+    return x + y:
+  }
+
